93611465650ebcf6028f520ce839d86ba35b4d96
whipstein <matt_braunstein@yahoo.com>
2024-12-08 11:23:57 -0600

    refactored code to calculate in rust

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,125 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use num_complex::Complex;
+
+fn get_mult(scale: &str) -> f64 {
+    match scale {
+        "tera" | "T" | "THz" | "thz" => 1e-12,
+        "giga" | "G" | "GHz" | "ghz" | "GΩ" => 1e-9,
+        "mega" | "M" | "MHz" | "mhz" | "MΩ" => 1e-6,
+        "kilo" | "k" | "kHz" | "khz" | "kΩ" => 1e-3,
+        "milli" | "m" | "mΩ" | "mF" | "mH" => 1e3,
+        "micro" | "u" | "μΩ" | "μF" | "μH" => 1e6,
+        "nano" | "n" | "nΩ" | "nF" | "nH" => 1e9,
+        "pico" | "p" | "pΩ" | "pF" | "pH" => 1e12,
+        "femto" | "f" | "fΩ" | "fF" | "fH" => 1e15,
+        _ => 1.0,
+    }
+}
+
+fn scale(val: f64, scale: &str) -> f64 {
+    val * get_mult(scale)
+}
+
+fn unscale(val: f64, scale: &str) -> f64 {
+    val / get_mult(scale)
+}
+
+#[derive(serde::Serialize, Default)]
+struct ResultsReturn {
+    zre: f64,
+    zim: f64,
+    gre: f64,
+    gim: f64,
+    gmag: f64,
+    gang: f64,
+    r: f64,
+    c: f64,
+}
+
+fn calc_gamma(z: Complex<f64>, z0: f64) -> Complex<f64> {
+    let z0: f64 = z0;
+
+    (z - z0) / (z + z0)
+}
+
+fn calc_z(gamma: Complex<f64>, z0: f64) -> Complex<f64> {
+    z0 * (1.0 + gamma) / (1.0 - gamma)
+}
+
+fn calc_rc(z: Complex<f64>, freq: f64, fscale: &str, rscale: &str, cscale: &str) -> (f64, f64) {
+    let y = 1.0 / z;
+
+    (
+        1.0 / scale(y.re, rscale),
+        scale(
+            y.im / (2.0 * std::f64::consts::PI * unscale(freq, fscale)),
+            cscale,
+        ),
+    )
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn calc_vals(
+    re: f64,
+    im: f64,
+    imp: &str,
+    z0: f64,
+    freq: f64,
+    fscale: &str,
+    rscale: &str,
+    cscale: &str,
+) -> Result<ResultsReturn, String> {
+    let mut out = ResultsReturn::default();
+
+    match imp {
+        "z" => {
+            let z = Complex::new(re, im);
+            out.zre = z.re;
+            out.zim = z.im;
+            let gamma = calc_gamma(z, z0);
+            out.gre = gamma.re;
+            out.gim = gamma.im;
+            out.gmag = gamma.norm();
+            out.gang = gamma.arg() * 180.0 / std::f64::consts::PI;
+            (out.r, out.c) = calc_rc(z, freq, fscale, rscale, cscale);
+        }
+        "ri" => {
+            let gamma = Complex::new(re, im);
+            out.gre = gamma.re;
+            out.gim = gamma.im;
+            out.gmag = gamma.norm();
+            out.gang = gamma.arg() * 180.0 / std::f64::consts::PI;
+            let z = calc_z(gamma, z0);
+            out.zre = z.re;
+            out.zim = z.im;
+            (out.r, out.c) = calc_rc(z, freq, fscale, rscale, cscale);
+        }
+        "ma" => {
+            let gamma = Complex::from_polar(re, im * std::f64::consts::PI / 180.0);
+            out.gre = gamma.re;
+            out.gim = gamma.im;
+            out.gmag = gamma.norm();
+            out.gang = gamma.arg() * 180.0 / std::f64::consts::PI;
+            let z = calc_z(gamma, z0);
+            out.zre = z.re;
+            out.zim = z.im;
+            (out.r, out.c) = calc_rc(z, freq, fscale, rscale, cscale);
+        }
+        "db" => {
+            let gamma =
+                Complex::from_polar(10_f64.powf(re / 20.0), im * std::f64::consts::PI / 180.0);
+            out.gre = gamma.re;
+            out.gim = gamma.im;
+            out.gmag = gamma.norm();
+            out.gang = gamma.arg() * 180.0 / std::f64::consts::PI;
+            let z = calc_z(gamma, z0);
+            out.zre = z.re;
+            out.zim = z.im;
+            (out.r, out.c) = calc_rc(z, freq, fscale, rscale, cscale);
+        }
+        _ => return Err("invalid impedance format".to_string()),
+    };
+
+    Ok(out)
 }
 
 use tauri::Manager;
@@ -15,7 +133,7 @@ pub fn run() {
             Ok(())
         })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![calc_vals])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,158 @@
 const { invoke } = window.__TAURI__.core;
 
-let greetInputEl;
-let greetMsgEl;
+let sig_digits = 2;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+function digits(val, sd) {
+    return val.toFixed(sd);
 }
 
+function print_unit(unit) {
+    switch (unit) {
+        case "milli":
+            return "m";
+        case "micro":
+            return "μ";
+        case "nano":
+            return "n";
+        case "pico":
+            return "p";
+        case "femto":
+            return "f";
+        default:
+            return "";
+    }
+}
+  
+function print_val(val, unit, suffix, sd) {
+    if (Number.isFinite(val)) {
+        return "" + digits(val, sd) + " " + print_unit(unit) + suffix;
+    }
+    return "" + val;
+}
+  
+function calcMatch() {
+    const num_format = document.getElementById("num_format").value;
+    const freq_unit = document.getElementById("freq_unit").value;
+    const cap_unit = document.getElementById("cap_unit").value;
+    const z0 = parseFloat(document.getElementById("z0").value);
+    const freq = parseFloat(document.getElementById("freq").value);
+    const re = parseFloat(document.getElementById("s11_re").value);
+    const im = parseFloat(document.getElementById("s11_im").value);
+
+    invoke("calc_vals", {re: re, im: im, imp: num_format, z0: z0, freq: freq, fscale: freq_unit, rscale: "", cscale: cap_unit})
+        .then((result) => {
+            var txt = "<div class=\"text_box\">" + print_val(result.zre, "", "", sig_digits);
+            if (result.zim < 0) txt += " - ";
+            else txt += " + ";
+            txt += print_val(Math.abs(result.zim), "", "j Ω</div>", sig_digits);
+            document.getElementById("z_val").innerHTML = txt;
+
+            var txt = "<div class=\"text_box\">" + print_val(result.gre, "", "", sig_digits);
+            if (result.gim < 0) txt += " - ";
+            else txt += " + ";
+            txt += print_val(Math.abs(result.gim), "", "j</div>", sig_digits);
+            document.getElementById("gamma_ri_val").innerHTML = txt;
+        
+            var txt = "<div class=\"text_box\">" + print_val(result.gmag, "", " &angmsd; ", sig_digits);
+            txt += print_val(result.gang, "", "&deg; </div>", sig_digits);
+            document.getElementById("gamma_ma_val").innerHTML = txt;
+        
+            var txt = "<div class=\"text_box\">" + print_val(result.r, "", " Ω</div>", sig_digits);
+            document.getElementById("r_val").innerHTML = txt;
+            var txt = "<div class=\"text_box\">" + print_val(result.c, cap_unit, "F</div>", sig_digits);
+            document.getElementById("c_val").innerHTML = txt;
+        })
+        .catch((err) => {
+            console.log("ERROR: " + err);
+            var txt = "<div class=\"text_box\">ERROR";
+            document.getElementById("z_val").innerHTML = txt;
+            document.getElementById("gamma_ri_val").innerHTML = txt;
+            document.getElementById("gamma_ma_val").innerHTML = txt;
+            document.getElementById("r_val").innerHTML = txt;
+            document.getElementById("c_val").innerHTML = txt;
+        });
+}
+
+function change_imp() {
+    const num_format = document.getElementById("num_format").value;
+
+    if (num_format == "z") {
+        document.getElementById("s11_re_label").textContent = "Z Real";
+        document.getElementById("s11_im_label").textContent = "Z Imaginary";
+    } else if (num_format == "ri") {
+        document.getElementById("s11_re_label").textContent = "Gamma Real";
+        document.getElementById("s11_im_label").textContent = "Gamma Imaginary";
+    } else if (num_format == "ma") {
+        document.getElementById("s11_re_label").textContent = "Gamma Magnitude";
+        document.getElementById("s11_im_label").textContent = "Gamma Angle (deg)";
+    } else if (num_format == "db") {
+        document.getElementById("s11_re_label").textContent = "Gamma Magnitude (dB)";
+        document.getElementById("s11_im_label").textContent = "Gamma Angle (deg)";
+    } else if (num_format == "rc") {
+        document.getElementById("s11_re_label").textContent = "Resistance";
+        document.getElementById("s11_im_label").textContent = "Capacitance (fF)";
+    }
+
+    calcMatch();
+}
+
+let numFormatEl;
+let capUnitEl;
+let freqUnitEl;
+let freqEl;
+let z0El;
+let sigDigitsEl;
+let s11ReEl;
+let s11ImEl;
+
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
+    numFormatEl = document.getElementById("num_format");
+    capUnitEl = document.getElementById("cap_unit");
+    freqUnitEl = document.getElementById("freq_unit");
+    freqEl = document.getElementById("freq");
+    z0El = document.getElementById("z0");
+    sigDigitsEl = document.getElementById("sig_digits");
+    s11ReEl = document.getElementById("s11_re");
+    s11ImEl = document.getElementById("s11_im");
+
+    numFormatEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        change_imp();
+    });
+
+    capUnitEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        calcMatch();
+    });
+
+    freqUnitEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        calcMatch();
+    });
+
+    freqEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        calcMatch();
+    });
+
+    z0El.addEventListener("change", (e) => {
+        e.preventDefault();
+        calcMatch();
+    });
+
+    sigDigitsEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        sig_digits = parseInt(sigDigitsEl.value, 10);
+        calcMatch();
+    });
+
+    s11ReEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        calcMatch();
+    });
+
+    s11ImEl.addEventListener("change", (e) => {
+        e.preventDefault();
+        calcMatch();
+    });
 });
